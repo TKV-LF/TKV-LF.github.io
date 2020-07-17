99be92517774900987f8c6f16ae8545eaa79243c
TKV-LF <1851161554@e.tlu.edu.vn>
2020-07-18 01:43:43 +0700

    sua lai chuc nang mo tab cua cac icon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 $(function(){
-    var flag=0;
+    $(".fa-facebook").click(function () { 
+        window.open("https://facebook.com/thuongkhungvu");
+        
+    });
+
+    $(".fa-twitter").click(function () { 
+        window.open("https://twitter.com/thuynt2000");
+        
+    });
+
+    $(".fa-github").click(function () { 
+        window.open("https://github.com/TKV-LF");
+        
+    });
+
     
+
+    var flag=0;
     $('.share').on('click',function(){
     if(flag == 0)
-        {
+        {;
         $(this).siblings('.one').animate({
         top:'450px',
         left:'50%',
